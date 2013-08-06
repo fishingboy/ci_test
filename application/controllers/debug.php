469ee873342ca62a1f0a2037a3d33051b6d7bb59
Leo Kuo <et282523@hotmail.com>
2013-08-06 18:07:59 +0800

    測試 ci parser 用法!!

--- application/controllers/debug.php
+++ application/controllers/debug.php
@@ -4,7 +4,16 @@ class Debug extends CI_Controller
 {
 	public function index()
 	{
+		// 載入 firephp
 		$this->load->helper("firephp");
+
+		// 載入 parser
+		$this->load->library("parser");
+
+		// 輸出到 firebug
 		fb_log($this);
+
+		// 說明
+		echo "請看 firebug 頁面的主控台！";
 	}
 }
