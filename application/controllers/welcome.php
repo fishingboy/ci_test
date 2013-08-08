fc1fe3fc6854b374d5e4bce9d580f7398f9dc6a0
wbkuo <et282523@hotmail.com>
2013-08-08 23:43:24 +0800

    還原 welcome.php

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -17,10 +17,8 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index($a='')
+	public function index()
 	{
-		// $this->load->helper("file",);
-	    echo "\$a = '$a'";
 		$this->load->view('welcome_message');
 	}
 }
