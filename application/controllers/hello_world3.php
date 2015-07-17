2b62d030801f328063f1d7ba8ba7d8560751aa0f
RAIN <rainlay@gmail.com>
2015-07-17 11:42:56 +0800

    test test

--- application/controllers/hello_world3.php
+++ application/controllers/hello_world3.php
@@ -1,13 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class hello_world3 extends CI_Controller 
+class hello_world3 extends CI_Controller
 {
 	public function index()
 	{
 		$data = array
 		(
 			'account' => 'wbkuo',
-			'name'    => '郭文彬'
+			'name'    => '郭文彬',
+			'msg'     => 'LEO好神'
 		);
 
 		$this->load->view('hello_world3', $data);
