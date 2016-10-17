bbff89a3c960aa5d24d2c1a3ea336ee8c9017400
Leo.Kuo <et282523@hotmail.com>
2016-10-17 10:06:59 +0800

    題庫轉成 json

--- application/controllers/quiz_convert.php
+++ application/controllers/quiz_convert.php
@@ -10,12 +10,22 @@ class Quiz_convert extends CI_Controller
     	parent::__construct();
     }
 
-	public function index()
-	{
+    public function index()
+    {
         header("Content-Type:text/html; charset=utf-8");
         $this->load->library("quiz_parser");
         $this->quiz_parser->setContent();
         $data = $this->quiz_parser->getData();
         echo "<pre>data = " . print_r($data, TRUE). "</pre>";
+    }
+
+	public function json()
+	{
+        header("Content-Type:text/html; charset=utf-8");
+        $this->load->library("quiz_parser");
+        $this->quiz_parser->setContent();
+        $data = $this->quiz_parser->getData();
+        // echo "<pre>data = " . print_r($data, TRUE). "</pre>";
+        echo json_encode($data);
 	}
 }
